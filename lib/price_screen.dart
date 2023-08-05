24ef27540015f326fcca034fe0b4fa172c7087b6
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-08-05 12:21:53 +0530

    Added for loop for showing all the currencies in dropdown

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -46,20 +47,11 @@ class _PriceScreenState extends State<PriceScreen> {
             child: DropdownButton<String>(
               value: selectedCurrency,
               items: [
-                DropdownMenuItem(
-                  child: Text(
-                    'USD',
+                for (var item in currenciesList)
+                  DropdownMenuItem(
+                    child: Text(item),
+                    value: item,
                   ),
-                  value: 'USD',
-                ),
-                DropdownMenuItem(
-                  child: Text('EUR'),
-                  value: 'EUR',
-                ),
-                DropdownMenuItem(
-                  child: Text('GBP'),
-                  value: 'GBP',
-                ),
               ],
               onChanged: (value) {
                 setState(() {
